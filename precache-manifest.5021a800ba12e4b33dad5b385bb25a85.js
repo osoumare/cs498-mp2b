c7ce31c32b17c94411a110435ce7be90791b8960
osoumare <37199015+osoumare@users.noreply.github.com>
2019-03-12 18:57:40 -0500

    Updates

---
+++ precache-manifest.5021a800ba12e4b33dad5b385bb25a85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cs498-mp2b/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "34cab86005a4055d36d6",
+    "url": "/cs498-mp2b/static/js/runtime~main.8c8d196a.js"
+  },
+  {
+    "revision": "8bc394e09460dec006e4",
+    "url": "/cs498-mp2b/static/js/main.0d8bea38.chunk.js"
+  },
+  {
+    "revision": "4b9c1c10499cebff2452",
+    "url": "/cs498-mp2b/static/js/2.5c53c58b.chunk.js"
+  },
+  {
+    "revision": "8bc394e09460dec006e4",
+    "url": "/cs498-mp2b/static/css/main.09146cea.chunk.css"
+  },
+  {
+    "revision": "e7e1db704ea0b6626104202195122059",
+    "url": "/cs498-mp2b/index.html"
+  }
+];
